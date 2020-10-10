06bd11a3650ec8029cc7fd4ea44e43360e35eec2
Luiz Pedro <luizpsmoreira@gmail.com>
2020-10-10 17:28:38 -0300

    Exibindo erros

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,12 +5,14 @@ let b7Validator= {
 
         let inputs = form.querySelectorAll('input');
 
+        b7Validator.clearErrors();
+
         for (let index = 0; index < inputs.length; index++) {
             const input = inputs[index];
             let check = b7Validator.checkInput(input);
             if (check !== true) {
                 send = false;
-                console.log(check);
+                b7Validator.showError(input, check);
             }
         }
 
@@ -39,6 +41,26 @@ let b7Validator= {
             }
         }
         return true;
+    },
+    showError:(input, error) => {
+        input.style.borderColor = '#f00';
+
+        let errorElement = document.createElement('div');
+        errorElement.classList.add('error');
+        errorElement.innerHTML = error;
+
+        input.parentElement.insertBefore(errorElement, input.ElementSibling);
+    },
+    clearErrors:() => {
+        let inputs = form.querySelectorAll('input');
+        for (let index = 0; index < inputs.length; index++) {
+            inputs[index].style = '';
+        }
+        
+        let errorElements = document.querySelectorAll('.error');
+        for (let index = 0; index < errorElements.length; index++) {
+            errorElements[index].remove();            
+        }
     }
 }
 
